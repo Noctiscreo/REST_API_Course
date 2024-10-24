8e7ed486043a8cb0d99df5ca5625fe7cde8a1d23
Noctiscreo <noctiscreo@gmail.com>
2024-10-24 16:03:40 +0100

    Default parameters.

--- pythonRefresher/17_default_parameter_values/code.py
+++ pythonRefresher/17_default_parameter_values/code.py
@@ -1,4 +1,7 @@
-def add(x, y):
+# cannot have a default parameter followed by a non-default parameter:
+def add(x=5, y):
     print(x + y)
 
-add(5, 8)
+# cannot put a positional argument after a keyword or named argument:
+add(x=5, 5)
+
