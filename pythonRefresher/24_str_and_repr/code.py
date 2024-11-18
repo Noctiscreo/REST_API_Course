9b009314b9141843f6314d0d80a643775f6c210a
Noctiscreo <noctiscreo@gmail.com>
2024-11-18 15:24:43 +0000

    Magic methods: __str__ and __repr__

---
+++ pythonRefresher/24_str_and_repr/code.py
@@ -0,0 +1,23 @@
+class Person:
+    # MAGIC METHODS are called automatically by Python in certin conditions.
+    # e.g. '__init__' is called by Python when instantiating a class.
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    # The __str__ MAGIC METHOD turns your object into a string:
+    def __str__(self):
+        return f"Person {self.name}, {self.age} years old."
+    
+    # The __str__ method will be printed out by default. 
+    # Comment it out to see the below.
+    # __repr__ is used by programmers to print 'unambiguous objects'.
+    def __repr__(self):
+        return f"<Person({self.name}, {self.age})>"
+    
+
+bob = Person("Bob", 35)
+
+# Prints the 'bob' object that changes to you use __str__.
+print(bob)
+
