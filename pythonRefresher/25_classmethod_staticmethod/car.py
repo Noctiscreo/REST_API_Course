53cc2e61cf93b4034a6c69a40f149398173806ed
Noctiscreo <noctiscreo@gmail.com>
2025-04-21 18:34:26 +0100

    Added class methods.

--- pythonRefresher/25_classmethod_staticmethod/car.py
+++ pythonRefresher/25_classmethod_staticmethod/car.py
@@ -5,13 +5,38 @@ class Car:
         self.make = make
         self.model = model
         self.year = year
+        self.odometer_reading = 1000
 
     def get_descriptive_name(self):
         """Return a neatly formatted descriptive name."""
         long_name = f"{self.year} {self.make} {self.model}"
         return long_name.title()
+    
+    def read_odometer(self):
+        """Print a statement showing the car's mileage."""
+        print(f"This car has {self.odometer_reading} miles on it.")
 
-my_new_car = Car('audi', 'a4', 2024)
+    def update_odometer(self, mileage):
+        """
+        Set the odometer reading to the given value.
+        Reject the change if it attempts to roll the odometer back.
+        """
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+    
+    def increment_odometer(self, miles):
+        """Add the given amount to the odometer reading."""
+        self.odometer_reading += miles
 
-print(my_new_car.get_descriptive_name())
+my_used_car = Car('subaru', 'outback', 2019)
+print(my_used_car.get_descriptive_name())
+
+my_used_car.update_odometer(23_500)
+my_used_car.read_odometer()
+
+my_used_car.increment_odometer(100)
+my_used_car.read_odometer()
+
 
